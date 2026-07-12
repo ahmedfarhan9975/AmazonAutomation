91f2aa1f0921851b173d3294d456b1411645ff5c
ahmedfarhan9975 <ahmedfarhan9975@gmail.com>
2026-07-12 18:59:03 +0530

    Update TestListener.java

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,51 +1,80 @@
+// Package where listener classes are stored
 package listeners;
 
+// Java imports for file handling
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+
+// Java imports for timestamp
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+// Selenium imports
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+// TestNG Listener imports
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+// Import BaseTest to access WebDriver
 import fixtures.BaseTest;
 
+// Custom Listener class
 public class TestListener implements ITestListener {
 
+    // This method automatically runs whenever a test fails
     @Override
     public void onTestFailure(ITestResult result) {
 
+        // Get the current WebDriver instance
         WebDriver driver = BaseTest.getDriver();
 
+        // If driver is null, do nothing
         if (driver == null) {
             return;
         }
 
-        File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // Capture screenshot
+        File source =
+                ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
-        String timeStamp = LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        // Generate timestamp
+        String timeStamp =
+                LocalDateTime.now().format(
+                        DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
 
-        File destination = new File(
-                "screenshots/" + result.getName() + "_" + timeStamp + ".png");
+        // Create screenshot file name
+        File destination =
+                new File("screenshots/"
+                        + result.getName()
+                        + "_"
+                        + timeStamp
+                        + ".png");
 
+        // Create screenshots folder if it doesn't exist
         destination.getParentFile().mkdirs();
 
         try {
 
-            Files.copy(source.toPath(), destination.toPath(),
+            // Copy screenshot to destination folder
+            Files.copy(source.toPath(),
+                    destination.toPath(),
                     StandardCopyOption.REPLACE_EXISTING);
 
-            System.out.println("Screenshot saved: "
-                    + destination.getAbsolutePath());
+            // Print screenshot location
+            System.out.println(
+                    "Screenshot saved: "
+                            + destination.getAbsolutePath());
 
-        } catch (IOException e) {
+        }
 
+        catch (IOException e) {
+
+            // Print error if copying fails
             e.printStackTrace();
 
         }
